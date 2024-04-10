204d479e0f6b19430b0fb52b02cbc318964ee185
Darya and Code story <daryaandcodestory@Fiodars-MacBook-Pro-4.local>
2024-04-10 18:44:02 -0400

    Defining and Using Dynamic Routers

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,3 +1,12 @@
 export default function ProductPage(){
-  return <p> my products</p>
-}
+  return(
+    <>
+    <h1> my products</h1>
+    <ul>
+      <li>Prod1</li>
+      <li>Prod2</li>
+      <li>Prod3</li>
+    </ul>
+    </>
+  )
+  }
